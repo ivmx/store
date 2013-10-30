55f5b55ea5a50478fd256f64f600463b30dea2ef
Luis Antonio Ramirez Martinez <luis190991@gmail.com>
2013-10-29 23:14:44 -0700

    Commit Inicial de la aplicación.

---
+++ app/controllers/Home.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package controllers;
+
+/**
+ *
+ * @author Luis
+ */
+public class Home extends Application{
+    
+    public static void index(){
+        render();
+    }
+    
+}
